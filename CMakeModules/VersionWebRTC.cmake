bd308f7d7e09509f60863a6693b689152a992021
mayeut <mayeut@users.noreply.github.com>
2017-08-19 05:52:42 +0200

    restore source directory for webrtc

--- CMakeModules/VersionWebRTC.cmake
+++ CMakeModules/VersionWebRTC.cmake
@@ -1,7 +1,7 @@
 include(LibWebRTCExecute)
 
 libwebrtc_execute(
-    COMMAND ${GIT_EXECUTABLE} clone ${LIBWEBRTC_WEBRTC_GIT} ${CMAKE_BINARY_DIR}/webrtc-version
+    COMMAND ${GIT_EXECUTABLE} clone ${LIBWEBRTC_WEBRTC_GIT} ${CMAKE_BINARY_DIR}/webrtc/src
     OUTPUT_VARIABLE _WEBRTC_CLONE
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     STAMPFILE webrtc-version-clone
@@ -12,7 +12,7 @@ libwebrtc_execute(
 libwebrtc_execute(
     COMMAND ${GIT_EXECUTABLE} config remote.origin.fetch +refs/branch-heads/*:refs/remotes/branch-heads/* ^\\+refs/branch-heads/\\*:.*$
     OUTPUT_VARIABLE _WEBRTC_CONFIG_FETCH
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc-version
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc/src
     STAMPFILE webrtc-version-config-fetch
     STATUS "Setting up branch-heads refspecs"
     ERROR "Unable to add branch-heads refspec to the git config"
@@ -21,7 +21,7 @@ libwebrtc_execute(
 libwebrtc_execute(
     COMMAND ${GIT_EXECUTABLE} fetch origin
     OUTPUT_VARIABLE _WEBRTC_FETCH
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc-version
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc/src
     STAMPFILE webrtc-version-fetch-branch-heads
     STATUS "Fetching webrtc"
     ERROR "Unable to fetch webrtc"
@@ -31,7 +31,7 @@ if (WEBRTC_REVISION)
   libwebrtc_execute(
       COMMAND ${GIT_EXECUTABLE} checkout ${WEBRTC_REVISION}
       OUTPUT_VARIABLE _WEBRTC_CHECKOUT
-      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc-version
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc/src
       STAMPFILE
       STATUS "Checking out webrtc to commit ${WEBRTC_REVISION}"
       ERROR "Unable to checkout webrtc to commit ${WEBRTC_REVISION}"
@@ -39,7 +39,7 @@ if (WEBRTC_REVISION)
   libwebrtc_execute(
       COMMAND ${GIT_EXECUTABLE} branch -r --contains ${WEBRTC_REVISION}
       OUTPUT_VARIABLE _WEBRTC_BRANCHES
-      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc-version
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc/src
       STAMPFILE
       STATUS "Get branches for ${WEBRTC_REVISION}"
       ERROR "Unable to get branches for ${WEBRTC_REVISION}"
@@ -51,7 +51,7 @@ elseif (WEBRTC_BRANCH_HEAD)
   libwebrtc_execute(
       COMMAND ${GIT_EXECUTABLE} fetch origin ${WEBRTC_BRANCH_HEAD}
       OUTPUT_VARIABLE _WEBRTC_FETCH
-      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc-version
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc/src
       STAMPFILE
       STATUS "Fetching ${WEBRTC_BRANCH_HEAD}"
       ERROR "Unable to fetch ${WEBRTC_BRANCH_HEAD}"
@@ -60,7 +60,7 @@ elseif (WEBRTC_BRANCH_HEAD)
   libwebrtc_execute(
       COMMAND ${GIT_EXECUTABLE} checkout FETCH_HEAD
       OUTPUT_VARIABLE _WEBRTC_CHECKOUT
-      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc-version
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc/src
       STAMPFILE
       STATUS "Checking out ${WEBRTC_BRANCH_HEAD}"
       ERROR "Unable to checkout ${WEBRTC_BRANCH_HEAD}"
@@ -69,7 +69,7 @@ elseif (WEBRTC_BRANCH_HEAD)
   libwebrtc_execute(
       COMMAND ${GIT_EXECUTABLE} log -1 --format=%H
       OUTPUT_VARIABLE _WEBRTC_SHA1
-      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc-version
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/webrtc/src
       STAMPFILE
       STATUS "Get commit sha1 for ${WEBRTC_BRANCH_HEAD}"
       ERROR "Unable to get commit sha1 for ${WEBRTC_BRANCH_HEAD}"
